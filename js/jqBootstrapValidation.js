629236391c54f729b5d2a459f27fd46b80a1a921
chrisjpn <chris.jameson@gmail.com>
2020-01-14 22:26:43 +0900

    fix chars

--- js/jqBootstrapValidation.js
+++ js/jqBootstrapValidation.js
@@ -183,7 +183,7 @@
             //                                                     EMAIL
             // ---------------------------------------------------------
             if ($this.attr("type") !== undefined && $this.attr("type").toLowerCase() === "email") {
-              message = "กรุณาใส่อีเมล์ที่ถูกต้อง<!-- data-validator-validemail-message to override -->";
+              message = "กรุณาใส่อีเมล์<!-- data-validator-validemail-message to override -->";
               if ($this.data("validationValidemailMessage")) {
                 message = $this.data("validationValidemailMessage");
               } else if ($this.data("validationEmailMessage")) {
